54733905be3aac0fa65d8c90c73a2da84a057d1e
moraneus <moraneus@gmail.com>
2023-08-29 10:20:49 +0300

    Update run_experiment files

--- src/test/scala/experiments/tp/example3/trace_generator.py
+++ src/test/scala/experiments/tp/example3/trace_generator.py
@@ -1,36 +1,9 @@
-import random
-from random import choices
-
-population = [
-    1, 2, 3, 4,
-    5, 6, 7, 8,
-    9, 10, 11, 12,
-    13, 14, 15, 16,
-    17, 18, 19, 20
-]
-
-weights = [
-    0.01, 0.01, 0.01, 0.01,
-    0.02, 0.02, 0.02, 0.05,
-    0.05, 0.05, 0.05, 0.05,
-    0.05, 0.05, 0.05, 0.1,
-    0.1, 0.1, 0.1, 0.1
-]
-
 user_input = input("Select the size of the trace: ")
 
 with open("log.csv", "w") as f:
     for i in range(1, int(user_input) + 1):
-        rand = choices(population, weights)[0]
-        if rand in (3, 7, 11, 17):
-            f.write(f"q,{rand},{random. randint(1, 10000)}\n")
+        if i % 1000 != 0:
+            f.write(f"p,{i}\n")
         else:
-            f.write(f"p,{rand}\n")
-            
-
-
-
-        
-        
-    
+            f.write(f"p,{0}\n")
 
