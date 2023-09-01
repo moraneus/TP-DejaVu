9a76513d59fa946c0c26d4f6f75952c92603f4ae
moraneus <moraneus@gmail.com>
2023-09-01 21:32:48 +0300

    Update example and experiment folder

--- src/test/scala/experiments/tp/example1/trace_generator.py
+++ src/test/scala/experiments/tp/example1/trace_generator.py
@@ -1,31 +1,13 @@
 import random
-from random import choices
-
-population = [
-    1, 2, 3, 4,
-    5, 6, 7, 8,
-    9, 10, 11, 12,
-    13, 14, 15, 16,
-    17, 18, 19, 20
-]
-
-weights = [
-    0.01, 0.01, 0.01, 0.01,
-    0.02, 0.02, 0.02, 0.05,
-    0.05, 0.05, 0.05, 0.05,
-    0.05, 0.05, 0.05, 0.1,
-    0.1, 0.1, 0.1, 0.1
-]
 
 user_input = input("Select the size of the trace: ")
 
 with open("log.csv", "w") as f:
     for i in range(1, int(user_input) + 1):
-        rand = choices(population, weights)[0]
-        if rand in (3, 7, 11, 17):
-            f.write(f"q,{rand},{random. randint(1, 10000)}\n")
+        if i in (3, 7, 11, 17, 20, 22, 30, 35, 41):
+            f.write(f"q,{random.randint(-1000000000, 1000000000)},{random. randint(1, 1000000000)}\n")
         else:
-            f.write(f"p,{rand}\n")
+            f.write(f"p,{random.randint(-1000000000, 1000000000)}\n")
             
 
 
--- src/test/scala/experiments/tp/example3/trace_generator.py
+++ src/test/scala/experiments/tp/example3/trace_generator.py
@@ -1,9 +1,11 @@
+import random
+
 user_input = input("Select the size of the trace: ")
 
 with open("log.csv", "w") as f:
     for i in range(1, int(user_input) + 1):
-        if i % 1000 != 0:
-            f.write(f"p,{i}\n")
+        if i % 100 == 0:
+            f.write(f"p,{random.randint(10000000, 20000000)}\n")
         else:
-            f.write(f"p,{0}\n")
+            f.write(f"q,{random.randint(0, 10000000)}\n")
 
